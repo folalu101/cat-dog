2d95973218c715f821acff78623a41ed84376bfe
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-19 19:27:17 +0100

    add more jquery code to js file for functionality of hello id

--- js/app.js
+++ js/app.js
@@ -9,4 +9,18 @@ $(document).ready(function(){
 
     });
 
+    $("#hello").click(function(){
+        $("#primary").append("<li>Hello Welcome</li>");
+        $("#secondary").append("<li>Hello Come in</li>");
+        $("#success").append("<li>Hello Honey !</li>");
+        $("#danger").append("<li>Hello Come on !!</li>");
+        $("#warning").append("<li>Hello Take care !</li>");
+
+    });
+
+    $("#yes").click(function(){
+        
+
+    });
+
 });
